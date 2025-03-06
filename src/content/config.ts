955870fc12357bb87a4751205e73e57677751017
Johathan29 <rosariojohathan@gmail.com>
2025-03-05 20:00:09 -0400

    install astro

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection,z } from "astro:content";
+
+
+const books=defineCollection({
+    schema:z.object({
+        title:z.string(),
+        author:z.string(),
+        readtime: z.number(),
+img:z.string(),
+description:z.string()
+
+    })
+})
+export const collections={books}
